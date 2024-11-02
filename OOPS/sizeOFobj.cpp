06122d70e55267b3314482bfdb4a8171777f2f46
Sachin-11111010011 <sachinadhikari0008@gmail.com>
2024-11-03 02:56:41 +0530

    SizeOfObj

---
+++ OOPS/sizeOFobj.cpp
@@ -0,0 +1,19 @@
+
+#include <bits/stdc++.h>
+using namespace std;
+class hello
+    {
+    //   int b;
+    //   int c;
+      char d;
+      char e;
+      int b;
+      char g;
+    };
+    
+int main()
+{
+    hello a;
+    cout<<sizeof(a)<<" "<<endl;
+    
+}
