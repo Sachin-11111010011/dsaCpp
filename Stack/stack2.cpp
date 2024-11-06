8b5a6daefb36ee95e4bb3659121d2b678d8d20a4
Sachin-11111010011 <sachinadhikari0008@gmail.com>
2024-11-06 22:29:38 +0530

    stack

--- Stack/stack2.cpp
+++ Stack/stack2.cpp
@@ -1,9 +1,31 @@
-Stack s(5); // Initialize stack with max size of 5
+#include <iostream>
+#include <stack>
+#include <sstream>
+#include <string>
+using namespace std;
 
-s.push(10);
-s.push(20);
-s.push(30);
-cout << s.peek() << endl;  // Output: 30
-s.pop();
-cout << s.peek() << endl;  // Output: 20
-cout << (s.isEmpty() ? "Empty" : "Not Empty") << endl;  // Output: Not Empty
+int evaluatePostfix(const string& expression) {
+    stack<int> s;
+    istringstream tokens(expression);
+    string token;
+    
+    while (tokens >> token) {
+        if (isdigit(token[0]) || (token.length() > 1 && token[0] == '-')) {  // Handle negative numbers
+            s.push(stoi(token));
+        } else {
+            int b = s.top(); s.pop();
+            int a = s.top(); s.pop();
+            if (token == "+") s.push(a + b);
+            else if (token == "-") s.push(a - b);
+            else if (token == "*") s.push(a * b);
+            else if (token == "/") s.push(a / b);
+        }
+    }
+    return s.top();
+}
+
+int main() {
+    string expression = "2 3 + 5 *";
+    cout << "Result: " << evaluatePostfix(expression) << endl;  // Output: 25
+    return 0;
+}
