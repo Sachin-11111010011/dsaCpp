9b6fe20f2c18a26c1c5bca42f6e4240448821315
Sachin-11111010011 <sachinadhikari0008@gmail.com>
2024-12-01 13:18:11 +0530

    1346. Check If N and Its Double Exist

--- LeetcodeDaily/18.cpp
+++ LeetcodeDaily/18.cpp
@@ -1,13 +1,31 @@
-class Solution {
-public:
-    bool checkIfExist(vector<int>& arr) {
-        int n=arr.size();
+// class Solution {
+// public:
+//     bool checkIfExist(vector<int>& arr) {
+//         int n=arr.size();
+//         for(int i=0;i<n;i++){
+//             for(int j=0;j<n;j++){
+//                 if(i!=j && arr[i]==2*arr[j]){
+//                     return true;
+//                 }
+//             }
+//         }
+//         return false;
+//     }
+// };
+
+class Solution{
+    public:
+    bool checkIfExist(vector<int>&arr){
+        int n= arr.size();
+        unordered_set<int>st;
+
         for(int i=0;i<n;i++){
-            for(int j=0;j<n;j++){
-                if(i!=j && arr[i]==2*arr[j]){
-                    return true;
-                }
+            if(st.find(arr[i]*2) != st.end()) {
+                return true;
+            } else if(arr[i]%2 ==0 && st.find(arr[i]/2) != st.end()){
+                return true;
             }
+            st.insert(arr[i])
         }
         return false;
     }
