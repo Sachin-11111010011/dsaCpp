2abc4b7303b319a911ea439b22fbadc6f2af983d
Sachin-11111010011 <sachinadhikari0008@gmail.com>
2024-12-11 18:30:21 +0530

    2779. Maximum Beauty of an Array After Applying Operation

---
+++ LeetcodeDaily/28.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int maximumBeauty(vector<int>& nums, int k) {
+        int n= nums.size();
+
+        sort(begin(nums),end(nums));
+        int i=0;
+        int j=0;
+
+        int maxBeauty=0;
+
+        while(i<n){
+            while(j<n && nums[j] <=nums[i]   +  2*k){
+                j++;
+            }
+            maxBeauty= max(maxBeauty, j-i);
+            i++;
+        }
+        return maxBeauty;
+    }
+};
