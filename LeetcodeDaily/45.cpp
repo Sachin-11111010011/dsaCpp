238075692821d011486c5ad014e28329c60f754a
Sachin-11111010011 <sachinadhikari0008@gmail.com>
2024-12-26 13:41:28 +0530

    494. Target Sum

---
+++ LeetcodeDaily/45.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+int S;
+int solve(vector<int>& nums, int target, int i, int currSum,vector<vector<int>>& t){
+    if(i==nums.size()){
+        if(currSum==target){
+            return 1;
+        }else{
+            return 0;
+        }
+    }
+    if(t[i][currSum + S] != -1){
+        return t[i][currSum + S];
+    }
+
+    int plus = solve(nums, target ,i+1, currSum +nums[i] , t);
+    int minus = solve(nums,target, i+1, currSum- nums[i] , t);
+    return t[i][currSum+S] = plus + minus;
+}
+
+    int findTargetSumWays(vector<int>& nums, int target) {
+        int n = nums.size();
+        S=accumulate(begin(nums) , end(nums),0);
+        vector<vector<int>> t(n+1 ,vector<int>(2*S+1,-1)  );
+        return solve(nums,target,0,0,t);
+    }
+};
