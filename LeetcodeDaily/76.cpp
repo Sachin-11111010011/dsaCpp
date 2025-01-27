27f92f5d9ab038ad995f08b5a7fb3c050eab2c46
Sachin-11111010011 <sachinadhikari0008@gmail.com>
2025-01-27 12:56:01 +0530

    1462. Course Schedule IV

---
+++ LeetcodeDaily/76.cpp
@@ -0,0 +1,42 @@
+class Solution {
+public:
+   
+   bool dfs(unordered_map<int, vector<int>>&adj , int src, int dest , vector<bool>& visited ){
+         visited[src] = true;
+
+         if(src == dest){
+            return true;  // yes we can reach to dest
+         }
+
+         bool isReachable =false;
+         for(auto &adjNode : adj[src]){
+            if(!visited[adjNode]){
+                isReachable = isReachable || dfs(adj , adjNode, dest, visited);
+            }
+         }
+         return isReachable;
+   }
+
+
+    vector<bool> checkIfPrerequisite(int numCourse, vector<vector<int>>& prerequisites, vector<vector<int>>& queries) {
+        unordered_map<int, vector<int>> adj;
+
+        for( auto &edge :prerequisites){
+            int u = edge[0];
+            int v = edge[1];
+
+            adj[u].push_back(v);  // u --> v
+        }
+        int Q = queries.size();
+        vector<bool> result(Q);
+
+        for( int i=0;i < Q;i++){
+            int u= queries[i][0];  // source
+            int v= queries[i][1];  // Dest
+
+            vector<bool>visited(numCourse , false);
+            result[i] =dfs(adj , u ,v, visited);  
+        }
+        return result;
+    }
+};
