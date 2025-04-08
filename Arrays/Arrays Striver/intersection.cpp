a2975c9f3b29ee604b5456bfc3b4f6c19b316630
Sachin-11111010011 <sachinadhikari0008@gmail.com>
2025-04-09 05:12:41 +0530

    intersection

---
+++ Arrays/Arrays Striver/intersection.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h> 
+vector<int> findArrayIntersection(vector<int> &arr1, int n, vector<int> &arr2, int m)
+{
+	vector<int> ans;
+
+	// int vis[m] ={0};
+	vector<int> vis(m, 0);
+	for(int i=0;i<n;i++){
+		for(int j=0;j<m;j++){
+			if(arr1[i]==arr2[j]  && vis[j]==0){
+				ans.push_back(arr1[i]);
+				vis[j]=1;
+				break;
+			}
+			if(arr2[j]>arr1[i]) break;
+		}
+	}
+	return ans;
+}
